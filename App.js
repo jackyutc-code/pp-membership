3a019be1eb9666d34611f889ee535872a4c82bab
Rhapsody60 <jackyutc@yahoo.com>
2026-01-25 14:40:40 -0800

    Fix App.js entry (remove app.json content)

--- App.js
+++ App.js
@@ -1,7 +1,3 @@
-import JoinScreen from "./src/screens/JoinScreen";
-
-import AdminAddLocationScreen from "./src/screens/AdminAddLocationScreen";
-
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -9,51 +5,34 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CardScreen from "./src/screens/CardScreen";
 import LocationsScreen from "./src/screens/LocationsScreen";
 import LocationDetailScreen from "./src/screens/LocationDetailScreen";
+import JoinScreen from "./src/screens/JoinScreen";
+import AdminAddLocationScreen from "./src/screens/AdminAddLocationScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
-{
-  "expo": {
-    "name": "P&P",
-    "slug": "pp-membership",
-    "version": "1.0.0",
-    "ios": {
-      "bundleIdentifier": "com.jackyu.pp",
-      "supportsTablet": true
-    }
-  }
-}
-
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-<Stack.Screen name="Join" component={JoinScreen} options={{ title: "Join" }} />
+      <Stack.Navigator screenOptions={{ headerTitleAlign: "center" }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ title: "P&P" }} />
+        <Stack.Screen name="Join" component={JoinScreen} options={{ title: "Join" }} />
 
-<Stack.Screen
-  name="AdminAddLocation"
-  component={AdminAddLocationScreen}
-  options={{ title: "Add Location" }}
-/> 
+        <Stack.Screen name="Card" component={CardScreen} options={{ title: "Membership Card" }} />
+        <Stack.Screen name="Locations" component={LocationsScreen} options={{ title: "Locations" }} />
+        <Stack.Screen name="LocationDetail" component={LocationDetailScreen} options={{ title: "Location" }} />
 
         <Stack.Screen
-          name="Card"
-          component={CardScreen}
-          options={{ title: "Membership Card" }}
+          name="AdminAddLocation"
+          component={AdminAddLocationScreen}
+          options={{ title: "Add Location" }}
         />
-        <Stack.Screen
-          name="Locations"
-          component={LocationsScreen}
-          options={{ title: "Locations" }}
-        />
-        <Stack.Screen
-          name="LocationDetail"
-          component={LocationDetailScreen}
-          options={{ title: "Location" }}
-        />
+
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "Profile" }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-<Stack.Screen name="Join" component={JoinScreen} options={{ title: "Join" }} />
+
 
